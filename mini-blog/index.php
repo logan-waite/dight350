30831f13fa996310c7102aac42cfae27421af6ba
Logan Waite <lwaite2010@gmail.com>
2016-02-12 14:58:38 -0700

    all posts formatting

--- mini-blog/index.php
+++ mini-blog/index.php
@@ -4,7 +4,7 @@
     </head>
     <body>
         <header>
-            <h1>Logan's Blog</h1>
+            <a href='index.php'><h1 class='title'>Logan's Blog</h1></a>
             <nav>
                 <ul>
                     <a href='posts.php'><li>All of My Posts</li></a>
@@ -14,8 +14,8 @@
         </header>
         <section>
             <div class='text-background'>
-                <h1>Blog Post the First</h1>
-                <p>I've got something important to say</p>
+                <h1 class='blog-header'>What are the Digital Humanities?</h1>
+                <p>The field of Digital Humanities could be split into two separate approaches to increasing knowledge in the humanities: the usage of digital advancements in technology to study older material, and the use of traditional techniques to study more contemporary subjects. While these fields are not the only ways to segment the fields, they constitute the main ways that many scholars of this day are approaching the constantly changing world of the humanities. In many ways, applying newer techniques to materials originally created and studied before the advent of computers increases the speed and effenciency with which new knowledge is garned. Coming from the other direction, as the digital world becomes more involved in the lives of humankind, tradional methods of study will allow us to better understand common events beyond what the newer methods can tell us.</p>
             </div>
         </section>
     </body>
---
+++ mini-blog/posts.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="resources/stylesheet.css">
+    </head>
+    <body>
+        <header>
+            <a href='index.php'><h1 class='title'>Logan's Blog</h1></a>
+            <nav>
+                <ul>
+                    <a href='documentation.php'><li>Database Queries</li></a>
+                </ul>
+            </nav>
+        </header>
+        <h1 class='page-title'> Examples of Digital Humanities</h1>
+        <section class='text-background'>
+            <div>
+                <h1 class='blog-header'>Google</h1>
+                <p>In the past, when writing a research paper for the humanities, you would need to go to the library and spend hours upon hours looking in card catalogs and periodicals trying to find relevant information for what you were researching. This could be frustrating when you would run into dead ends, and was frigteningly ineffecient in finding information that was actually relevant. Now, however, web search engines have indexed a vast amount of the web, and work has been done to archive many of the materials that previously were only available in a physical form. From the comfort of your desk, you can search for information and can even view articles and artifacts that originated from the other side of the world. By applying a new technology to traditional processes, we are able to greatly facilitate the study of humanties.</p>
+            </div>
+            <div>
+                <h1 class='blog-header'>Example 2</h1>
+                <p></p>
+            </div>
+        </section>
+    </body>
+</html>
