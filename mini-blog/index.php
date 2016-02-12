b96ee9080dc84cda2866fd80745c19c97953fb4b
Logan Waite <lwaite2010@gmail.com>
2016-02-12 12:53:09 -0700

    setup

---
+++ mini-blog/index.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="resources/stylesheet.css">
+    </head>
+    <body>
+        <header>
+            <h1>Logan's Blog</h1>
+            <nav>
+                <ul>
+                    <a href='posts.php'><li>All of My Posts</li></a>
+                    <a href='documentation.php'><li>Database Queries</li></a>
+                </ul>
+            </nav>
+        </header>
+        <section>
+            <div class='text-background'>
+                <h1>Blog Post the First</h1>
+                <p>I've got something important to say</p>
+            </div>
+        </section>
+    </body>
+</html>
